1f9c164e9b64b48273cb18e7461f10f460345259
Nithin_Dell_G15 <shanmukhsainithin@gmail.com>
2025-09-11 01:03:42 +0530

    revamped UI

--- composeApp/src/commonMain/kotlin/com/nithin/roomcmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/nithin/roomcmp/App.kt
@@ -4,22 +4,31 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.nithin.roomcmp.HomeSreen.HomeScreen
 import com.nithin.roomcmp.components.TaskItem
 import com.nithin.roomcmp.data.Task
 import com.nithin.roomcmp.data.TaskDatabase
@@ -43,7 +52,7 @@ fun App(
     RecomposeCounter("App")
 
 
-    val taskItems by taskViewModel.taskItems.collectAsState()
+
 
     MaterialTheme {
 
@@ -53,26 +62,42 @@ fun App(
             }
         }
 
-        Scaffold { paddingValues ->
-
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-
-                itemsIndexed(taskItems){ index, item->
-                    TaskItem(
-                        task = item,
-                        onDeleteIconClick = { task ->
-                            taskViewModel.deleteTask(task)
-                        },
-                        index = index,
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(0xFFF3F4F6)),
+            floatingActionButton = {
+                Surface(
+                    modifier = Modifier,
+                    shape = CircleShape,
+                    color = Color.Black
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add",
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .size(24.dp),
+                        tint = Color.White
                     )
                 }
+            }
+        ) { paddingValues ->
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = paddingValues.calculateTopPadding(),
+                        bottom = paddingValues.calculateBottomPadding()
+                    )
+                    .padding(horizontal = 24.dp)
+            ){
+
+                HomeScreen()
 
             }
+
         }
 
 
